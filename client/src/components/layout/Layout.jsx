8b2e310e8a83ef722d3d02e14c08fb9671ce98a3
YunThpy <103516331+YunThpy@users.noreply.github.com>
2024-09-19 21:42:55 +0900

    router.js파일에 layout.jsx 최상단적용
    .

--- client/src/components/layout/Layout.jsx
+++ client/src/components/layout/Layout.jsx
@@ -1,10 +1,12 @@
 // 어떤 형식인지 지정하고 만들면 될 것 같아요
 import React from 'react';
+import { Outlet } from 'react-router-dom';
 
 const Layout = () => {
     return (
         <div>
-            레이아웃
+            dldslsdl
+            <Outlet />
         </div>
     );
 };
--- client/src/routes/router.js
+++ client/src/routes/router.js
@@ -1,5 +1,6 @@
 import { createBrowserRouter, RouterProvider, Route, Navigate, Outlet } from "react-router-dom";
 
+import Layout from "../components/layout/Layout";
 import MainPage from "../pages/mainpage/MainPage";
 
 import LoginPage from "../pages/loginpage/LoginPage";
@@ -41,115 +42,120 @@ const ProtectedRoute = ({ element }) => {
   };
 
 const router = createBrowserRouter([
-
     {
         path: "/",
-        element: <MainPage />,
-    },
+        element: <Layout />,
+        children: [
+    
+        {
+            path: "/",
+            element: <MainPage />,
+        },
         
-    {
-        path: "/login",
-        element: <LoginPage />,
-        children: [
-            {
-              path: "signup",
-              element: <SignUpPage />,
-            },
-          ],
-    },
+        {
+            path: "/login",
+            element: <LoginPage />,
+            children: [
+                {
+                  path: "signup",
+                  element: <SignUpPage />,
+                },
+              ],
+        },
     
-    {
-        path: "/bookclubs",
-        element: <BookClubPage />,
-        children: [
-            {
-              path: "bookclubinfo/:bookclubid",
-              element: <BookClubDetailPage />,
-            },
-          ],
-    },
+        {
+            path: "/bookclubs",
+            element: <BookClubPage />,
+             children: [
+                {
+                 path: "bookclubinfo/:bookclubid",
+                 element: <BookClubDetailPage />,
+                },
+           ],
+        },
 
-    {
-        path: "/feeds",
-        element: <FeedPage />,
-    },
+        {
+           path: "/feeds",
+           element: <FeedPage />,
+        },
     
-    {
-        path: "/books",
-        element: <LibraryPage />,
-        children: [
-            {
-              path: "bookinfo/:bookid",
-              element: <BookInfoPage />,
-            },
-          ],
-    },
+        {
+           path: "/books",
+           element: <LibraryPage />,
+            children: [
+                {
+                  path: "bookinfo/:bookid",
+                 element: <BookInfoPage />,
+              },
+             ],
+        },
 
-    {
-        path: "/posts",
-        element: <PostPage />,
-        children: [
-            {
-              path: "postinfo/:postid",
-              element: <PostDetailPage />,
-            },
-          ],
-    },
+        {
+            path: "/posts",
+            element: <PostPage />,
+            children: [
+                {
+                  path: "postinfo/:postid",
+                  element: <PostDetailPage />,
+                },
+              ],
+        },
 
-    {
-        path: "/search",
-        element: <SearchPage />,
+        {
+            path: "/search",
+            element: <SearchPage />,
         
-    },
+        },
 
-    // 로그인 페이지로 리다이렉션 되는 url들 - 시작
+        // 로그인 페이지로 리다이렉션 되는 url들 - 시작
     
-    {
-        path: "/mybookclub",
-        element: <ProtectedRoute element={<MyBookClubPage />} />,
-        children: [
-            {
-              path: "makingclub",
-              element: <ProtectedRoute element={<MakingBookClubPage />} />,
-            },
-          ],
-    },
+        {
+            path: "/mybookclub",
+            element: <ProtectedRoute element={<MyBookClubPage />} />,
+            children: [
+                {
+                  path: "makingclub",
+                  element: <ProtectedRoute element={<MakingBookClubPage />} />,
+                },
+              ],
+        },
 
-    {
-        path: "/mylibrary",
-        element: <ProtectedRoute element={<MyLibraryPage />} />,
-        children: [
-            {
-                path: "bookrecord",
-                element: <ProtectedRoute element={<BookRecordPage />} />,
-            },
+        {
+            path: "/mylibrary",
+            element: <ProtectedRoute element={<MyLibraryPage />} />,
+            children: [
+                {
+                    path: "bookrecord",
+                    element: <ProtectedRoute element={<BookRecordPage />} />,
+                },
 
-            {
-                path: "posting",
-                element: <ProtectedRoute element={<PostingPage />} />,
-            },
-        ],
-    },
-    
-    {
-        path: "/mypage",
-        element: <ProtectedRoute element={<MyPage />} />,
-        children: [
-            {
-                path: "updateprofile",
-                element: <ProtectedRoute element={<UpdateProfilePage />} />,
-            },
-        ],
-    },
+                {
+                    path: "posting",
+                    element: <ProtectedRoute element={<PostingPage />} />,
+                },
+            ],
+        },
+        
+        {
+            path: "/mypage",
+            element: <ProtectedRoute element={<MyPage />} />,
+            children: [
+                {
+                    path: "updateprofile",
+                    element: <ProtectedRoute element={<UpdateProfilePage />} />,
+                },
+            ],
+        },
 
 
-    // 로그인 페이지로 리다이렉션 되는 url들 - 끝 
-
-    {
-        path: "*",
-        element: <PageNotFound />,
-    },
-
+            // 로그인 페이지로 리다이렉션 되는 url들 - 끝 
+        
+        {
+            path: "*",
+            element: <PageNotFound />,
+        },
+        ]
+    }
 ])
 
 
