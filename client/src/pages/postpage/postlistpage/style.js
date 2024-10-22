e026062de17170d62c887f5cb47c2a3dd3781286
yujeong <yujeong3389@gmail.com>
2024-10-22 22:58:00 +0900

    Error

--- client/src/pages/postpage/postlistpage/style.js
+++ client/src/pages/postpage/postlistpage/style.js
@@ -99,7 +99,6 @@ S.PostItem = styled.div`
     padding: 1.5%;
     background-color: white;
     border-radius: 0.5rem;
-    
     box-shadow: 0 2px 8px rgba(0, 0, 0, 0.1);
     width: 100%;
     /* height:100%; */
