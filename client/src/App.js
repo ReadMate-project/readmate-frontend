3cbe391eb79dc953e0cb6632f17ecad3100a0be5
yujeong3389 <159121446+yujeong3389@users.noreply.github.com>
2024-11-30 23:50:00 +0900

    vercel.json 삭제

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,6 @@
 import './App.css';
+// import { RouterProvider} from "react-router-dom";
+import router from "./routes/router";
 import GlobalStyle from './global/global';
 import { ThemeProvider } from 'styled-components';
 import theme from './global/theme';
