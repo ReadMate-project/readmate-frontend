8974fc8a342860d001a1251b92db56f49d439346
yujeong3389 <159121446+yujeong3389@users.noreply.github.com>
2024-11-30 23:53:02 +0900

    vercel.json삭제하니까 됨

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,10 @@
 import './App.css';
-// import { RouterProvider} from "react-router-dom";
+import { RouterProvider} from "react-router-dom";
 import router from "./routes/router";
 import GlobalStyle from './global/global';
 import { ThemeProvider } from 'styled-components';
 import theme from './global/theme';
 import { UserProvider } from './context/UserContext';
-import Router from './routes/router';
 
 
 
@@ -15,7 +14,7 @@ function App() {
     <UserProvider> 
       <ThemeProvider theme={theme}>
         <GlobalStyle />
-        <Router/>
+        <RouterProvider router={router} />
       </ThemeProvider>
     </UserProvider>
   );
--- client/src/routes/router.js
+++ client/src/routes/router.js
@@ -1,21 +1,36 @@
-import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
+import { createBrowserRouter, RouterProvider, Route, Navigate, Outlet } from "react-router-dom";
+
 import Layout from "../components/layout/Layout";
 import MainPage from "../pages/mainpage/MainPage";
+
 import LoginPage from "../pages/loginpage/loginPage/LoginPage";
+
 import LibraryPage from "../pages/librarypage/LibraryPage";
 import BookInfoPage from "../pages/librarypage/bookinfopage/BookInfoPage";
 import BookClubPage from "../pages/bookclubpage/BookClubPage";
 import BookClubDetailPage from "../pages/bookclubpage/bookclubdetailpage/BookClubDetailPage";
+
 import PostPage from "../pages/postpage/postlistpage/PostPage";
 import PostDetailPage from "../pages/postpage/postdetailpage/PostDetailPage";
+
 import SearchPage from "../pages/searchpage/SearchPage";
+
+
+// 로그인 필요 페이지 
+import { isAuthenticated } from "../auth";
+
 import MyPage from "../pages/mypage(Auth)/mypage/MyPage";
 import UpdateProfilePage from "../pages/mypage(Auth)/updateprofilepage(Auth)/UpdateProfilePage";
+
 import MyLibraryPage from "../pages/mylibrarypage(Auth)/MyLibraryPage";
 import BookRecordPage from "../pages/mylibrarypage(Auth)/bookrecordpage(Auth)/BookRecordPage";
 import PostingPage from "../pages/mylibrarypage(Auth)/postingpage(Auth)/PostingPage";
+
+
 import MyBookClubPage from "../pages/mybookclubpage(Auth)/MyBookClubPage";
 import MakingBookClubPage from "../pages/mybookclubpage(Auth)/makingbookclubpage(Auth)/MakingBookClubPage";
+// 로그인 필요 페이지
+
 import PageNotFound from "../components/PageNotFound";
 import KakaoRedirect from "../pages/loginpage/loginPage/KakaoRedirect";
 import CreateNickNamePage from "../pages/loginpage/loginPage/CreateNickNamePage";
@@ -31,80 +46,185 @@ import EssayList from "../pages/essaypage/essayListPage/EssayList";
 import ModifyPost from "../pages/postpage/modifyPostPage/ModifyPost";
 import EssayDetailPage from "../pages/essaypage/essayDetailPage/EssayDetailPage";
 import BookCalendarDetail from "../pages/bookcalendarpage/Detail/BookCalendarDetail";
-import { isAuthenticated } from "../auth";
+
 
 const ProtectedRoute = ({ element }) => {
-  return isAuthenticated() ? element : <Navigate to="/login" />;
-};
+    return isAuthenticated() ? element : <Navigate to="/login" />;
+  };
 
-function Router() {
-  return (
-    <HashRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          {/* 메인 페이지 */}
-          <Route index element={<MainPage />} />
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Layout />,
+        children: [
+    
+        {
+            path: "/",
+            element: <MainPage />,
+        },
+        {
+            path: "/login",
+            element: <LoginMain />,
+            children:[
+                {
+                    path: "/login",
+                    element: <LoginPage />,
+                    
+                },
+                {
+                    path:"/login/account",
+                    element: <CreateNickNamePage />,
+                    
+                },
+            ]
+            
+        },
+        {
+            path:"/api/v1/auth/oauth2/kakao/code",
+            element:<KakaoRedirect/>
+        },
+        {
+            path: "/bookclubs",
+            element: <BookClubPage />,
+             children: [
+                {
+                 path: "bookclubinfo/:bookclubid",
+                 element: <BookClubDetailPage />,
+                },
+           ],
+        },
 
-          {/* 로그인 */}
-          <Route path="/login" element={<LoginMain />}>
-            <Route index element={<LoginPage />} />
-            <Route path="account" element={<CreateNickNamePage />} />
-          </Route>
-          <Route path="/api/v1/auth/oauth2/kakao/code" element={<KakaoRedirect />} />
+        {
+           path: "/essay",
+           element: <EssayMain/>,
+           children:[
+            {
+                path:"/essay",
+                element:<EssayList/>
+            },
+            {
+                path:"/essay/essaydetail",
+                element:<EssayDetailPage/>
+            },
+            {
+                path: "/essay/createEssay",
+                element: <ProtectedRoute element={<PostingPage />} />,
+            },
+           ]
+        },
+    
+        {
+            path: "/books",
+            element: <LibraryPage />,
+        }, 
+        
+        {
+            path: "/books/bookinfo/:isbn",
+            element: <BookInfoPage />,
+              
+        },
+             
 
-          {/* 북클럽 */}
-          <Route path="/bookclubs" element={<BookClubPage />}>
-            <Route path="bookclubinfo/:bookclubid" element={<BookClubDetailPage />} />
-          </Route>
+        {
+            path: "/posts",
+            element: <PostMain />,
+            children: [
+                {
+                    path: "/posts",
+                    element: <PostPage />,
+                },
+                {
+                //   path: "postinfo/:postid",
+                    path:"/posts/postdetail",
+                    element: <PostDetailPage />,
+                },
+                {//내가 쓴 postdetail일 때만 보이게 해야함
+                        path:"/posts/postdetail/modifypost",
+                        element: <ProtectedRoute element={<ModifyPost />} />,
+                },
+                {
+                    path: "/posts/createPost",
+                    element: <ProtectedRoute element={<CreatePostPage />} />,
+                },
+                {
+                    path: "/posts/myPost",
+                    element: <ProtectedRoute element={<MyPost/>} />,
+                },
+              ],
+        },
 
-          {/* 에세이 */}
-          <Route path="/essay" element={<EssayMain />}>
-            <Route index element={<EssayList />} />
-            <Route path="essaydetail" element={<EssayDetailPage />} />
-            <Route path="createEssay" element={<ProtectedRoute element={<PostingPage />} />} />
-          </Route>
+        {
+            path: "/search",
+            element: <SearchPage />,
+        
+        },
 
-          {/* 라이브러리 */}
-          <Route path="/books" element={<LibraryPage />} />
-          <Route path="/books/bookinfo/:isbn" element={<BookInfoPage />} />
+        // 로그인 페이지로 리다이렉션 되는 url들 - 시작
+    
+        {
+            path: "/mybookclub",
+            element: <ProtectedRoute element={<MyBookClubPage />} />,
+            children: [
+                {
+                  path: "makingclub",
+                  element: <ProtectedRoute element={<MakingBookClubPage />} />,
+                },
+              ],
+        },
 
-          {/* 포스트 */}
-          <Route path="/posts" element={<PostMain />}>
-            <Route index element={<PostPage />} />
-            <Route path="postdetail" element={<PostDetailPage />} />
-            <Route path="postdetail/modifypost" element={<ProtectedRoute element={<ModifyPost />} />} />
-            <Route path="createPost" element={<ProtectedRoute element={<CreatePostPage />} />} />
-            <Route path="myPost" element={<ProtectedRoute element={<MyPost />} />} />
-          </Route>
+        {
+            path: "/mylibrary",
+            element: <ProtectedRoute element={<MyLibraryPage />} />,
+            children: [
+                {
+                    path: "/mylibrary",
+                    element: <ProtectedRoute element={<BookRecordPage />} />,
+                },
 
-          {/* 검색 */}
-          <Route path="/search" element={<SearchPage />} />
+                
+            ],
+        },
+        {
+            path: "/bookcalendar",
+            element:<ProtectedRoute element={<BookCalendarMain />} />,
+            children: [
+                {
+                    path: "/bookcalendar",
+                    element:<ProtectedRoute element={<BookCalendar />} />,
+                },
 
-          {/* 로그인 필수 */}
-          <Route path="/mybookclub" element={<ProtectedRoute element={<MyBookClubPage />} />}>
-            <Route path="makingclub" element={<ProtectedRoute element={<MakingBookClubPage />} />} />
-          </Route>
+                {
+                    path: "/bookcalendar/detail",
+                    element:  <ProtectedRoute element={<BookCalendarDetail />} />,
+                },
+            ],
+        },
+        
+        {
+            path: "/mypage",
+            element: <ProtectedRoute element={<MyPageMain/>} />,
+            children: [
+                {
+                    path: "/mypage",
+                    element: <ProtectedRoute element={<MyPage />} />,
+                    
+                },
+                {
+                    path: "/mypage/updateprofile",
+                    element: <ProtectedRoute element={<UpdateProfilePage />} />,
 
-          <Route path="/mylibrary" element={<ProtectedRoute element={<MyLibraryPage />} />}>
-            <Route index element={<ProtectedRoute element={<BookRecordPage />} />} />
-          </Route>
+                },
+            ],
+        },
 
-          <Route path="/bookcalendar" element={<ProtectedRoute element={<BookCalendarMain />} />}>
-            <Route index element={<ProtectedRoute element={<BookCalendar />} />} />
-            <Route path="detail" element={<ProtectedRoute element={<BookCalendarDetail />} />} />
-          </Route>
 
-          <Route path="/mypage" element={<ProtectedRoute element={<MyPageMain />} />}>
-            <Route index element={<ProtectedRoute element={<MyPage />} />} />
-            <Route path="updateprofile" element={<ProtectedRoute element={<UpdateProfilePage />} />} />
-          </Route>
-
-          {/* 404 페이지 */}
-          <Route path="*" element={<PageNotFound />} />
-        </Route>
-      </Routes>
-    </HashRouter>
-  );
-}
-
-export default Router;
+            // 로그인 페이지로 리다이렉션 되는 url들 - 끝 
+        
+        {
+            path: "*",
+            element: <PageNotFound />,
+        },
+        ]
+    }
+])
+export default router;
